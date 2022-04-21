b23e245af71ba0901241c52576fcb9ea62146bc8
combustiblemon <comblmn@gmail.com>
2022-04-21 12:06:04 +0300

    finished the demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,70 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import Card from '@/components/card/Card';
-import { CardWrapper, Form, Wrapper } from '@/styles/Index.styles';
-import { Button, FormControl, FormLabel, Input } from '@chakra-ui/react';
+import { CardWrapper, Form, Header, Wrapper } from '@/styles/Index.styles';
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+} from '@chakra-ui/react';
 import { NextPage } from 'next/types';
-import { useState } from 'react';
 import { useForm, ValidateResult } from 'react-hook-form';
 
 import FormElement from '../components/form-element/FormElement';
 
 const mockAPIData = [
   {
-    name: 'Card 1',
-    id: 'card1',
+    name: 'North',
+    id: 'north',
   },
   {
-    name: 'Card 2',
-    id: 'card2',
+    name: 'West',
+    id: 'west',
   },
   {
-    name: 'Card 3',
-    id: 'card3',
+    name: 'East',
+    id: 'east',
   },
   {
-    name: 'Card 4',
-    id: 'card4',
+    name: 'South',
+    id: 'south',
   },
 ];
 
-const hasUnder20Chars = (value: string): Promise<ValidateResult> => {
+const hasUnder20Chars = (value: string): ValidateResult => {
   let result: ValidateResult = true;
   if (value?.length > 20) {
     result = 'The maximum number of characters is 20.';
   }
   // eslint-disable-next-line no-promise-executor-return
+  return result;
+};
+
+const isUsernameAvailable = (value: string): Promise<ValidateResult> => {
+  let result: ValidateResult = true;
+  if (value.toLowerCase() === 'username') {
+    result = 'Username is not available.';
+  }
   return new Promise((resolve) => setTimeout(() => resolve(result), 0));
+};
+
+const isValidGender = (value: string): ValidateResult => {
+  let result: ValidateResult = true;
+  if (value === 'dog') {
+    result = 'You are not a dog...';
+  }
+  return result;
 };
 
 const initialValues = {
   username: '',
+  age: '',
   selectedCardId: '',
+  gender: 'male',
 };
 
 const HookForm: NextPage = () => {
-  // const [selectedCardID, setSelectedCardID] = useState<string>('');
-
   const {
     handleSubmit,
     register,
@@ -51,18 +72,19 @@ const HookForm: NextPage = () => {
     setValue,
     getValues,
     watch,
-    // Validation will trigger on the submit event and invalid inputs will attach onChange event listeners to re-validate them.
-    // delayError will delay the error state to be displayed to the end-user in milliseconds.
-    // Correcting the error input will remove the error instantly and delay will not be applied.
   } = useForm({
+    // Validation will trigger on the submit event and invalid inputs will
+    // attach 'onChange' event listeners to re-validate them.
     mode: 'onSubmit',
+    // delayError will delay the error state to be displayed to the end-user by milliseconds.
+    // Correcting the error input will remove the error instantly and delay will not be applied.
     delayError: 0,
+    // The default values used for initialization
     defaultValues: initialValues,
   });
 
   function onSubmit(values: any) {
     return new Promise<void>((resolve) => {
-      // values.selectedCardID = selectedCardID;
       setTimeout(() => {
         alert(JSON.stringify(values, null, 2));
         resolve();
@@ -73,36 +95,73 @@ const HookForm: NextPage = () => {
   return (
     <Wrapper>
       <Form onSubmit={handleSubmit(onSubmit)}>
-        {/* pass the error to FormControl so it can invalidate the input if error exists */}
+        <Header>React-hook-form with Chakra</Header>
+        {/* Pass the error to 'FormControl' so it can invalidate the input if error exists */}
         <FormControl isInvalid={Boolean(errors.username)}>
-          {/* pass the error to FormElement so it can display the error if it exists */}
+          {/* Pass the error to 'FormElement' so it can display the error if it exists */}
           <FormElement error={errors.username?.message}>
             <FormLabel htmlFor="username">First name</FormLabel>
+            {/* This is a text input */}
             <Input
               colorScheme="blackAlpha"
               variant="filled"
               id="username"
               placeholder="Username"
+              // 'register' takes the name of the field (same as the one in 'initialValues')
+              // and an object with the validations and various utilities
+              // more info: https://react-hook-form.com/api/useform/register
               {...register('username', {
-                required: 'This is required',
+                required: 'This is required.',
                 minLength: { value: 4, message: 'Minimum length is 4.' },
-                validate: hasUnder20Chars,
+                validate: {
+                  under20Chars: hasUnder20Chars,
+                  available: isUsernameAvailable,
+                },
+              })}
+            />
+          </FormElement>
+        </FormControl>
+        <FormControl isInvalid={Boolean(errors.age)}>
+          <FormElement error={errors.age?.message}>
+            <FormLabel htmlFor="age">What is your age?</FormLabel>
+            {/* This is a number input */}
+            <Input
+              variant="filled"
+              colorScheme="blackAlpha"
+              id="age"
+              type="number"
+              placeholder="Age"
+              {...register('age', {
+                required: { value: true, message: 'This is required.' },
+                min: {
+                  value: 18,
+                  message: 'You must be over 18 years old to sign up.',
+                },
               })}
             />
           </FormElement>
         </FormControl>
         <FormElement error={errors.selectedCardId?.message}>
+          <FormLabel htmlFor="direction">Where are you from?</FormLabel>
+          {/* This is a collection of cards you can select */}
           <CardWrapper
+            id="direction"
             {...register('selectedCardId', {
-              required: { value: true, message: 'hello i am error' },
+              required: { value: true, message: 'This is required.' },
             })}
           >
             {mockAPIData.map((value) => {
               return (
                 <Card
+                  key={value.id}
                   onClick={() =>
-                    setValue('selectedCardId', value.id, { shouldTouch: true })
+                    // We have to use 'setValue' in 'onClick' since this is not an input
+                    setValue('selectedCardId', value.id, {
+                      shouldTouch: true,
+                      shouldValidate: true,
+                    })
                   }
+                  // 'watch' is used to trigger a re-render on the component
                   selected={watch('selectedCardId') === value.id}
                 >
                   {value.name}
@@ -111,13 +170,30 @@ const HookForm: NextPage = () => {
             })}
           </CardWrapper>
         </FormElement>
+        <FormControl isInvalid={Boolean(errors.gender)}>
+          <FormElement error={errors.gender?.message}>
+            <FormLabel htmlFor="gender">Select your gender</FormLabel>
+            <Select
+              variant="filled"
+              colorScheme="blackAlpha"
+              id="gender"
+              {...register('gender', {
+                validate: isValidGender,
+              })}
+            >
+              <option value="male">Male</option>
+              <option value="Female">Female</option>
+              <option value="dog">Dog</option>
+            </Select>
+          </FormElement>
+        </FormControl>
         <Button
           mt={4}
           colorScheme="teal"
           isLoading={isSubmitting}
           type="submit"
         >
-          Submit
+          Sign Up
         </Button>
       </Form>
     </Wrapper>
--- styles/Index.styles.ts
+++ styles/Index.styles.ts
@@ -1,14 +1,22 @@
 import styled from 'styled-components';
 
+export const Wrapper = styled.div`
+  min-width: 500px;
+`;
+
 export const Form = styled.form`
   background-color: lightgray;
   padding: 30px;
 `;
 
-export const Wrapper = styled.div`
-  width: 100%;
+export const CardWrapper = styled.div`
+  margin: 5px 0px;
+  border: 1px solid black;
+  border-radius: 8px;
+  padding: 0px 5px;
 `;
 
-export const CardWrapper = styled.div`
-  margin: 15px 0px;
+export const Header = styled.h1`
+  font-size: 20px;
+  font-weight: 700;
 `;
