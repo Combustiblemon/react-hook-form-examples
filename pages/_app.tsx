7450c1ddbe51c910781b485785437168693dbd75
combustiblemon <comblmn@gmail.com>
2022-04-21 09:24:12 +0300

    set up chakra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
 
-export default MyApp
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+};
+
+export default MyApp;
